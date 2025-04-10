31784ef632e6706056048e7b4e683e8179c53e9a
Arthur <artu.donascimento@gmail.com>
2025-04-10 03:01:00 -0300

    começando o projeto

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("alura filmes");
+        System.out.println("filme: Top Gun Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = false;
+        double nota = 8.1;
+
+        if (incluidoNoPlano){
+            System.out.println("filme incluso no plano");
+        }else {
+            System.out.println("filme não incluso no plano");
+        }
+
+        System.out.println("nota: " + nota);
+
+    }
+}
+
