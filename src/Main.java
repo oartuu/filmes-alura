5d76497b350503772eb15f91eaa9c172f9a80eef
Arthur <artu.donascimento@gmail.com>
2025-04-19 13:48:21 -0300

    inalizando oprojeto, com loops e arrays

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("alura filmes");
-        System.out.println("filme: Top Gun Maverick");
 
-        int anoDeLancamento = 2022;
-        System.out.println("ano de lançamento: " + anoDeLancamento);
-        boolean incluidoNoPlano = false;
-        double nota = 8.1;
+        String[] movies  =  {"Top gun: maverick", "Star Wars: revenge of the sith"};
+        String[] movies_desc = {"a 2022 action drama film about a US Navy Captain who returns to Top Gun to train a group of younger pilots", "is a 2005 film that concludes the Star Wars prequel trilogy. It tells the story of Anakin Skywalker's turn to the dark side,\n the rise of the Galactic Empire, and the fall of the Jedi Order"};
+        int[] movies_stars = {7, 8};
 
-        if (incluidoNoPlano){
-            System.out.println("filme incluso no plano");
-        }else {
-            System.out.println("filme não incluso no plano");
+        System.out.println("alura movies");
+        System.out.println("movies: " + movies[0] + "  " + movies[1] + "\n" );
+
+
+        for (int i=0; i<2; i++){
+
+            System.out.println("movie: "+ movies[i]  );
+            System.out.println("description: " + movies_desc[i]);
+            System.out.println("stars: " + movies_stars[i]/2);
+            System.out.println("\n");
         }
-
-        System.out.println("nota: " + nota);
 
     }
 }
